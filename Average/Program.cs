6b5733c5b49d53b7e4c1331fc92f3a06229170b6
eugbor <eugeneborodin@mail.ru>
2016-10-03 01:59:19 +0300

    init

---
+++ Average/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Average
+{
+    class Program
+    {/// <summary>
+     /// Cреднее арифметическое через цикл do while.
+     /// </summary>
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int counter = 0;
+            double sum = 0;
+            double average = 0;
+
+            do
+            {
+                a = int.Parse(Console.ReadLine());
+                if (a != 0) { counter++; }
+                sum += a;
+            } while (a != 0);
+            average = sum / counter;
+
+            Console.WriteLine("количество чисел: {0}\n" +
+                              "общая сумма: {1}\n" +
+                              "среднее арифметическое: {2:0.00}", counter, sum, average);
+            Console.ReadLine();
+        }
+    }
+}
